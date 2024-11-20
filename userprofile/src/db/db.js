ac9143a50aac270a9374de28bfbda9175ca004b1
aghamarsh-v <2023mt93313@wilp.bits-pilani.ac.in>
2024-11-20 22:21:41 +0530

    Enable docker based deployment and online based mongodb

--- userprofile/src/db/db.js
+++ userprofile/src/db/db.js
@@ -1,6 +1,6 @@
 const Mongoose = require("mongoose")
 
-const localDB = 'mongodb://localhost:27017/bms_user_db'
+const localDB = 'mongodb+srv://2023mt93313:dbpass_123@bookmanagement.h2hjm.mongodb.net/bms_user_db?retryWrites=true&w=majority&appName=BookManagement'
 const connectDB = async function () {
     await Mongoose.connect(localDB, {
         useNewUrlParser: true,
